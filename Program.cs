6cc85c0855b0a396e05bed51e1455030f406fef5
Pablo Luiz <80119424+devpabloluiz@users.noreply.github.com>
2022-01-14 20:27:16 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using static System.Console;
+
+public class Program
+{
+    static void TrocarNome(Pessoa p1, string nomeNovo)
+    {
+        p1.Nome = nomeNovo;
+    }
+    public static void Main()
+    {
+        Pessoa p1 = new Pessoa();
+        p1.Nome = "Pablo";
+        p1.Idade = 21;
+        p1.Documento = "123456789";
+
+        TrocarNome(p1, "Matheus");
+
+        WriteLine($"O novo nome é: {p1.Nome}");
+    }
+}
